a6cede8a1fc3ca90cdad826af340cbb0947b13b3
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2021-07-29 22:26:07 +0900

    コマンドライン引数で TimeZone を指定できるようにした

--- lib/edate/cli.ex
+++ lib/edate/cli.ex
@@ -3,8 +3,9 @@ defmodule Edate.CLI do
   @moduledoc """
   """
 
-  def main(_argv) do
-    Edate.edate()
+  def main(argv) do
+    argv1 = if length(argv) < 1, do: "Asia/Tokyo", else: hd argv
+    Edate.edate argv1
   end
 end
 
